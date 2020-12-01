3f1f85f313d336128b8cf2bf7723156924e83fe7
Swatantra <swatantramishra1@gmail.com>
2020-12-01 11:44:34 +0530

    third commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import expenseReducer from '../components/data/expenseManagerSlice';
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     expense: expenseReducer
   },
 });
--- src/components/body/body.component.jsx
+++ src/components/body/body.component.jsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Modal } from 'react-responsive-modal';
+
+import { selectListData, selectModalStatus, selectTotalExpenses, updateExpenses, updateModalStatus } from "../data/expenseManagerSlice";
 import { ExpenseManagerComponent } from "./expense_manager/expense_manager.component"
 import { NoEntryComponent } from "./no-entry/no.entry.component"
-import { Modal } from 'react-responsive-modal';
-import "react-responsive-modal/styles.css";
-import { useDispatch, useSelector } from "react-redux";
-import { selectListData, selectModalStatus, selectTotalExpenses, updateExpenses, updateModalStatus } from "../data/expenseManagerSlice";
 import { ListComponent } from "./list/list.component";
+
+import "react-responsive-modal/styles.css";
 
 
 export const BodyComponent = () => {
@@ -19,13 +21,23 @@ export const BodyComponent = () => {
 
     const [showList, setShowList] = useState(false);
 
+    const [todayDate, setTodayDate] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+
 
     const dispatch = useDispatch();
     const expenses = useSelector(selectTotalExpenses);
     const selectList = useSelector(selectListData);
     const selectModalCurrentStatus = useSelector(selectModalStatus);
+
+    useEffect(() => {
+        let date = new Date();
+        date = date.toISOString().substring(0,10);
+        setTodayDate(date);
+    },[])
  
     const onCloseModal = () => {
+        setErrorMessage("");
         dispatch(updateModalStatus(false))
     }
 
@@ -68,47 +80,78 @@ export const BodyComponent = () => {
     }
 
 
-    const onSave = () => {
-        let amountData = 0;
-        if(paid) {
-            amountData = Number(expenses.paid) + Number(amount);
-        } else {
-            amountData =  Number(expenses.recieve) +  Number(amount)
+    const validation = () => {
+        if(!paid && !recieve) {
+            setErrorMessage("Please select Transaction Type");
+            return false;
         }
 
-        const data = {
-            totalPaid: paid ? amountData : expenses.paid,
-            totalRecieve: recieve ? amountData : expenses.recieve,
-          }
-
-          const listData = JSON.parse(JSON.stringify(selectList));
-          const randomId = Math.random() + "d";
-          const dataObj =  {
-            "transaction-type": paid ? "paid" : "recieve",
-            "name":name,
-            "discription": discription,
-            "amount": amount,
-            "date": date,
-            id: randomId
-        };
-        
-        if(listData && listData.hasOwnProperty(date)) {
-            listData[date].push(
-                dataObj
-            )
-        } else {
-            listData[date] ={};
-            listData[date] = [dataObj]
+        if(name.trim().length === 0) {
+            setErrorMessage("Enter name")
+            return false;
         }
-        
 
-        data.list = listData;
+        if(amount === 0) {
+            setErrorMessage("Enter valid amount")
+            return false;
+        }
 
-        dispatch(updateExpenses(data))
-        onCloseModal();
-        setInitialState();
-        setShowList(true)
+        if(!date) {
+            setErrorMessage("Select valid date")
+            return false;
+        }
+
+        setErrorMessage("");
+        return true;
     }
+
+
+    const onSave = () => {
+
+        if(validation()) {
+            let amountData = 0;
+            if(paid) {
+                amountData = Number(expenses.paid) + Number(amount);
+            } else {
+                amountData =  Number(expenses.recieve) +  Number(amount)
+            }
+    
+            const data = {
+                totalPaid: paid ? amountData : expenses.paid,
+                totalRecieve: recieve ? amountData : expenses.recieve,
+              }
+    
+              const listData = JSON.parse(JSON.stringify(selectList));
+              const randomId = Math.random() + "d";
+              const dataObj =  {
+                "transaction-type": paid ? "paid" : "recieve",
+                "name":name,
+                "discription": discription,
+                "amount": amount,
+                "date": date,
+                id: randomId
+            };
+            
+            if(listData && listData.hasOwnProperty(date)) {
+                listData[date].push(
+                    dataObj
+                )
+            } else {
+                listData[date] ={};
+                listData[date] = [dataObj]
+            }
+            
+    
+            data.list = listData;
+    
+            dispatch(updateExpenses(data))
+            onCloseModal();
+            setInitialState();
+            setShowList(true)
+        }  
+    }
+
+
 
 
 
@@ -126,36 +169,41 @@ export const BodyComponent = () => {
             <div className="formContainer">
                 <div className="input">
                  <div className="label">Transaction Type</div>
+                   <div className="checkbox-container"> 
                     <div className="checkbox">
                     <input type="checkbox" id="paid" name="paid" value="paid" onChange={onSetPaid} /><label for="paid"> You Paid</label>
                     </div>
                     <div className="checkbox">
                     <input type="checkbox" id="paid" name="took" value="took" onChange={onSetRecieve}  /><label for="took"> You Took</label>
-                    </div>                    
+                    </div>
+                     </div>                   
                 </div>   
 
                 <div className="input">
                  <div className="label">Name</div>
-                 <input type="text" id="name" name="name" value={name} onChange={(e) => onChangeNameInput(e)} />
+                 <input type="text" id="name" name="name" placeholder="Type Name" value={name} onChange={(e) => onChangeNameInput(e)} />
                 </div>   
 
                 <div className="input">
                  <div className="label">Amount</div>
-                 <input type="number" id="amount" name="amount" value={amount} onChange={(e) => onChangeAmountInput(e)} />
+                 <input type="number" id="amount" name="amount" placeholder="Amount" value={amount} onChange={(e) => onChangeAmountInput(e)} />
                 </div>   
 
                 <div className="input">
                  <div className="label">Discription</div>
-                 <input type="text" id="disc" name="disc" value={discription} onChange={(e) => onChangeDiscInput(e)} />
+                 <input type="text" id="disc" name="disc" value={discription} placeholder="Discription" onChange={(e) => onChangeDiscInput(e)} />
                 </div>   
 
                 <div className="input">
                  <div className="label">Date</div>
-                 <input type="date" id="date" name="date" value={date} onChange={(e) => onChangeDateInput(e)} />
+                 <input type="date" id="date" name="date" value={date} max={todayDate} onChange={(e) => onChangeDateInput(e)} />
                 </div>  
+                {errorMessage.length > 0 ? <div className="error"> {errorMessage}
+                </div> : ""}
+                
 
                 <div className="action">
-                    <button onClick={onSave}>Save</button>
+                    <button className="save" onClick={onSave}>Save</button>
                     <button onClick={onCloseModal}>Cancel</button>
                 </div>
 
